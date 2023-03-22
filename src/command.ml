c89a786103e936511c66b85c68649743e8904aec
Kevin Lin <kl628@cornell.edu>
2023-03-21 23:04:03 -0400

    Created "home" command function - Added code for type command and have one command availible, home - Filtered out spaces so edge cases would be covered - Used pattern matching to check what input it is

--- src/command.ml
+++ src/command.ml
@@ -1,3 +1,17 @@
+type object_phrase = string list
+
 type command =
-  | Hello
-  | Bye
+  | Home
+
+exception Empty
+exception Malformed
+
+let input_to_list input =
+  let lst = String.split_on_char ' ' input in
+  List.filter (fun x -> x <> "") lst
+
+let parse input = match input_to_list input with
+| [] -> raise Empty
+| h::t when ((h = "home") && (List.length t == 0)) -> Printf.printf "Result: %s\n"
+| _ -> raise Malformed
+

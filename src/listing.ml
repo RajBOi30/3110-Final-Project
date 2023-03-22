4e069189ec6c5b09337c13d67447ed5f6cce8d76
Kaylin Chan <kmc362@cornell.edu>
2023-03-21 23:56:54 -0400

    Add helper functions to listing.ml
    - add functions to get each record field of a listing

--- src/listing.ml
+++ src/listing.ml
@@ -26,3 +26,24 @@ let listing_from_json json =
 
 let feed_from_json json : f =
   { feed = json |> member "listings" |> to_list |> List.map listing_from_json }
+
+(**[get_listing_id x] returns the listing id of listing [x].*)
+let get_listing_id x = x.listing_id
+
+(**[get_user_id x] returns the user id of listing [x].*)
+let get_user_id x = x.user_id
+
+(**[get_username x] returns the user id of listing [x].*)
+let get_username x = x.username
+
+(**[get_title] returns the title of listing [x].*)
+let get_title x = x.title
+
+(**[get_desc] returns the description of listing [x].*)
+let get_desc x = x.description
+
+(**[get_price] returns the price of listing [x].*)
+let get_price x = x.price
+
+(**[get_date] returns the date of listing [x].*)
+let get_date x = x.date

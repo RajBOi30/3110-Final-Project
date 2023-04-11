a938361629ee693e9978ccd235abe0b3d6c1eb16
Kaylin Chan <kmc362@cornell.edu>
2023-04-11 17:33:59 -0400

    Added Command Test Cases

--- src/command.mli
+++ src/command.mli
@@ -2,5 +2,11 @@ type command =
   | Home
   | Quit
 
+exception Empty
+(** Raised when an empty command is parsed. *)
+
+exception Malformed
+(** Raised when a malformed command is parsed. *)
+
 val input_to_list : string -> string list
 val parse : string -> command
